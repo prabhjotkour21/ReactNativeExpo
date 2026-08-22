03d321a0e6af919e687ca8cead7079c6167298f2
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-08-22 13:15:48 +0530

    Update home screen

--- my-first-app/src/app/index.tsx
+++ my-first-app/src/app/index.tsx
@@ -1,98 +1,97 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
-import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
-
-function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
-  if (Device.isDevice) {
-    return (
-      <ThemedText type="small">
-        shake device or press <ThemedText type="code">m</ThemedText> in terminal
-      </ThemedText>
-    );
-  }
-  const shortcut = Platform.OS === 'android' ? 'cmd+m (or ctrl+m)' : 'cmd+d';
-  return (
-    <ThemedText type="small">
-      press <ThemedText type="code">{shortcut}</ThemedText>
-    </ThemedText>
-  );
-}
-
 export default function HomeScreen() {
+
   return (
-    <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
 
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
+    <SafeAreaView>
+    <ScrollView>
+    <View style={{
+      margin:30,
+      padding: 20, borderColor: "black", borderWidth: 2
+    }}>
+      <Text>Local Image</Text>
+      <Image
+        source={require("../../assets/images/profile.png")}
+        style={{
+          width: 150,
+          height:150
+        }}
+      
+      />
+      <Text>Remote Image</Text>
+      <Image
+        source={{ uri: "https://plus.unsplash.com/premium_photo-1787303328188-87352d89bfa8?q=80&w=992&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", }}
+        style={{
+          width: 150,
+          height:150
 
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
+        }}
+      />
+      <Text style={{fontSize:24, fontWeight:"bold"}}>Name :Jot kour</Text>
+      <Text style={{color:"red"}}>React Native Developer</Text>
+      <Text>Learning React Native</Text>
+      <Text>Text 1</Text>
+      <Text>Text 2</Text>
+      <Text>Text 3</Text>
+      <Text>Text 4</Text>
+      <Text>Text 5</Text>
+      <Text>Text 6</Text>
+      <Text>Text 7</Text>
+      <Text>Text 8</Text>
+      <Text>Text 9</Text>
+      <Text>Text 15</Text>
+      <Text>Text 16</Text>
+      <Text>Text 17</Text>
+      <Text>Text 18</Text>
+      <Text>Text 19</Text>
+      </View>
+      <ScrollView horizontal>
+      
+      <View  style={{
+            width: 150,
+            height: 150,
+            margin: 10,
+            borderWidth: 2,
+            borderColor: 'black',
+          }}><Text>Text 20</Text></View>
+      <View  style={{
+            width: 150,
+            height: 150,
+            margin: 10,
+            borderWidth: 2,
+            borderColor: 'black',
+          }}><Text>Text 21</Text></View>
+      <View  style={{
+            width: 150,
+            height: 150,
+            margin: 10,
+            borderWidth: 2,
+            borderColor: 'black',
+          }}><Text>Text 22</Text></View>
+      <View  style={{
+            width: 150,
+            height: 150,
+            margin: 10,
+            borderWidth: 2,
+            borderColor: 'black',
+          }}><Text>Text 23</Text></View>
+      <View  style={{
+            width: 150,
+            height: 150,
+            margin: 10,
+            borderWidth: 2,
+            borderColor: 'black',
+          }}><Text>Text 24</Text></View>
+      <View  style={{
+            width: 150,
+            height: 150,
+            margin: 10,
+            borderWidth: 2,
+            borderColor: 'black',
+        }}><Text>Text 25</Text></View>
+        </ScrollView>
+      </ScrollView>
       </SafeAreaView>
-    </ThemedView>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
-  },
-  safeArea: {
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    alignItems: 'center',
-    gap: Spacing.three,
-    paddingBottom: BottomTabInset + Spacing.three,
-    maxWidth: MaxContentWidth,
-  },
-  heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    paddingHorizontal: Spacing.four,
-    gap: Spacing.four,
-  },
-  title: {
-    textAlign: 'center',
-  },
-  code: {
-    textTransform: 'uppercase',
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: 'stretch',
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
-  },
-});
