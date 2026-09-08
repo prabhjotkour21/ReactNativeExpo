dd14eb2b457e5c503cf27d9e832658aaa33a66dd
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-09-08 17:12:09 +0530

    Add app layout

---
+++ jobApp/src/app/_layout.tsx
@@ -0,0 +1,20 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+    return (
+        <Stack>
+             <Stack.Screen
+                name="login"
+                options={{
+                    headerShown: false
+                }
+                }
+        
+            />
+        
+            
+            
+            
+        </Stack>
+    )
+}
