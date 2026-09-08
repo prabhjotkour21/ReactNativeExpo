1f594bc68bd87c3c1c6764fa7a863cbf28f273bb
prabhjotkour21 <kaurprabhjot.dev@gmail.com>
2026-09-08 17:12:43 +0530

    Add jobs screen

---
+++ jobApp/src/app/tabs/jobs.tsx
@@ -0,0 +1,98 @@
+import { router } from "expo-router";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10,
+    padding: 20,
+  },
+
+  containerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+
+  jobItem: {
+    marginBottom: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+export default function Jobs() {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const handleApi = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts",
+        );
+
+        const data = await response.json();
+
+        
+
+        setJobs(data);
+      } catch (error) {
+        setError("Something went wrong");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    handleApi();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.containerText}>Jobs Page</Text>
+
+      {loading && <Text>Loading...</Text>}
+
+      {error && <Text>{error}</Text>}
+
+      <FlatList
+        data={jobs}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.jobItem}>
+              <Text
+                onPress={() => {
+                  router.push(
+                    {
+                      pathname: "/job-details",
+                      params: {
+                        jobId:item.id.toString()
+                      }
+                    }
+                  );
+                }}
+              >
+                {item.id}
+              </Text>
+
+              <Text style={styles.title}>{item.title}</Text>
+
+              <Text>{item.body}</Text>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+}
